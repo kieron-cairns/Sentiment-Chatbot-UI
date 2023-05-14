641a66d0312be8d542917dee49a89be931a19fe2
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-14 18:01:47 +0100

    CHANGE: ChatForm compoenent added to App main entry page along with initial implementation of user and chatbot message submissions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ChatForm from './Components/ChatForm';
 
-function App() {
+const App = () => {
+
+  const [messages, setMessages] = useState([]);
+
+  const handleMessageSubmit = (messageContent) => {
+    const newMessage = {
+      content: messageContent,
+      sender: 'user',
+    };
+
+    setMessages([...messages, newMessage]);
+
+    // Simulate bot response (replace with your own logic)
+    setTimeout(() => {
+      const botResponse = {
+        content: 'This is a sample bot response.',
+        sender: 'bot',
+      };
+
+      setMessages([...messages, newMessage, botResponse]);
+    }, 500);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ChatForm />
   );
 }
 
