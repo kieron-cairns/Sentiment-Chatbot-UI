fca9050b5d2e7803ef415bb3a38e202a6a403fd9
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-20 14:34:38 +0100

    CHANGE: Auto scroll to bottm when new chat input is submitted

--- src/Components/ChatMessages.jsx
+++ src/Components/ChatMessages.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const ChatMessages = ({ inputMessage, messages }) => {
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="chatbot-messages">
       {messages.map((message, index) => (
-        <div className='loadedHistoryChat'
-          key={index}
-          style={{
-            
-            textAlign: 'center' // Center the text
-          }}
-        >
+        <div className="loadedHistoryChat" key={index} style={{ textAlign: 'center' }}>
           <div>
-          {message.queryText !== null && message.queryText !== '' && (
-          <div className="history-user-query">{message.queryText}</div>
-        )}
-
+            {message.queryText !== null && message.queryText !== '' && (
+              <div className="history-user-query">{message.queryText}</div>
+            )}
           </div>
-         {message.queryResult !== null ? (
-          <div className='history-bot-response'>
-          {message.queryResult}
-          </div>
-         ) : null}
+          {message.queryResult !== null ? (
+            <div className="history-bot-response">{message.queryResult}</div>
+          ) : null}
         </div>
       ))}
+      <div ref={messagesEndRef} /> {/* Scroll to this element */}
     </div>
   );
 };
