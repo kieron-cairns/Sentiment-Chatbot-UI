ec2020f17be9cec233063eb12d8b4195769cafae
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-21 12:31:52 +0100

    CHANGE: Login modal UI finished

--- src/Components/SignInModal.jsx
+++ src/Components/SignInModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const SignInModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [modalIsOpen, setIsOpen] = React.useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
   const handleSignIn = () => {
@@ -14,20 +14,45 @@ const SignInModal = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
+
+  const loginModalStyle = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0)"
+    },
+    content: {
+      top: "50%",
+      left: "55%",
+      width: "50vh",
+      height: "35vh",
+      right: "auto",
+      bottom: "auto",
+      textAlign: "center",
+      marginRight: "-50%",
+      borderRadius: "15px",
+      transform: "translate(-50%, -50%)"
+    }
+  };
+
   
   return (
-    <Modal className='outer-sign-in-modal' isOpen={isOpen} onRequestClose={closeModal}>
-        <div className='sign-in-modal'>
-      {isLoggedIn ? (
-        <h2>Welcome, user!</h2>
-      ) : (
-        <div>
-          <h2>Sign In</h2>
-          <button onClick={handleSignIn}>Sign In</button>
-        </div>
-      )}
-      </div>
-    </Modal>
+    <Modal
+    isOpen={modalIsOpen}
+    style={loginModalStyle}
+    closeTimeoutMS={300}
+    onRequestClose={() => setIsOpen(false)}
+  >
+    <div>
+    <h2>Please Login</h2>
+    </div>
+    <div className='login-modal-form'>
+    <input className='login-modal-input' placeholder='Username'></input>
+    </div>
+    <div>
+    <input className='login-modal-input' placeholder='Password'></input>
+    </div>
+    <button className='login-modal-button'>Login</button>
+
+  </Modal>
   );
 };
 
