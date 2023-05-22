3b2539c00939ee7344085f0514cd43574dff7ef8
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-22 19:52:38 +0100

    CHANGE: Form submission for user name and password complete

--- src/Components/SignInModal.jsx
+++ src/Components/SignInModal.jsx
@@ -3,43 +3,55 @@ import Modal from 'react-modal';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
-
-
-
-async function authenticateUser(username, password) {
-  try {
-
-    const headers = {
-
-      'username' : username,
-      'password' : password
-      // Add more headers as needed
-    };
-
-    const response = await axios.post('https://text-sentiment-analyser-web-api.azurewebsites.net/AuthenticateUser', {}, {
-      headers: headers
-    });
-    
-    const { token } = response.data;
-    
-    // Store the token in local storage
-    localStorage.setItem('token', response.data);
-    console.log('***** token is *****');
-    console.log(response.data);
-    // Decode the token to access its payload
-    // const decodedToken = jwt_decode(token);
-    // console.log('***** Decoded Token Is: *****');
-    // console.log(decodedToken);
-    
-  } catch (error) {
-    console.error('Authentication failed:', error);
-  }
-}
-
-
 const SignInModal = () => {
   const [modalIsOpen, setIsOpen] = React.useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+     const authenticateUser = async (event) => {
+      event.preventDefault();
+
+      try {
+    
+        const headers = {
+    
+          'username' : username,
+          'password' : password
+          // Add more headers as needed
+        };
+    
+        const response = await axios.post('https://text-sentiment-analyser-web-api.azurewebsites.net/AuthenticateUser', {}, {
+          headers: headers
+        });
+        
+        const { token } = response.data;
+        
+        // Store the token in local storage
+        localStorage.setItem('token', response.data);
+        console.log('***** token is *****');
+        console.log(response.data);
+        // Decode the token to access its payload
+        // const decodedToken = jwt_decode(token);
+        // console.log('***** Decoded Token Is: *****');
+        // console.log(decodedToken);
+        
+      } catch (error) {
+        console.error('Authentication failed:', error);
+      }
+    }
+    
+
+     // Function to handle form submission
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Call your function and pass the username and password
+      // Here, you can perform any required actions with the values
+      console.log('Username:', username);
+      console.log('Password:', password);
+};
   
   const handleSignIn = () => {
     // Implement your sign-in logic here
@@ -77,16 +89,15 @@ const SignInModal = () => {
     closeTimeoutMS={300}
     onRequestClose={() => setIsOpen(false)}
   >
-    <div>
+    <form onSubmit={authenticateUser}>
     <h2>Please Login</h2>
-    </div>
-    <div className='login-modal-form'>
-    <input className='login-modal-input' placeholder='Username'></input>
-    </div>
-    <div>
-    <input className='login-modal-input' placeholder='Password'></input>
-    </div>
-    <button onClick={authenticateUser} className='login-modal-button'>Login</button>
+    {/* <div className='login-modal-form'> */}
+    <input className='login-modal-input' value={username}  placeholder='Username' type='text' onChange={(e) => setUsername(e.target.value)}></input>
+    {/* </div>
+    <div> */}
+    <input className='login-modal-input' value={password} placeholder='Password' type='password' onChange={(e) => setPassword(e.target.value)}></input>
+    <button type='submit' className='login-modal-button'>Login</button>
+    </form>
 
   </Modal>
   );
